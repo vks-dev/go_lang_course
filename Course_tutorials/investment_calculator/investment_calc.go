6c1c9fc7253272096c0575da5351836459570871
venkat.in2002 <venkat.in2002@gmail.com>
2025-06-03 11:47:17 +0000

    Adjusted the final amount to inflation

--- Course_tutorials/investment_calculator/investment_calc.go
+++ Course_tutorials/investment_calculator/investment_calc.go
@@ -6,17 +6,19 @@ import (
 )
 
 func main() {
+	const inflationRate float64= 6.8
 	var principalAmount, years float64 = 10000, 10
 	interestRate := 12.8
 	// principalAmount, years, interestRate := 10000.0, 10.0, 12.8         Alternate Notiation for variable declaration
 
 	var finalAmount = principalAmount * math.Pow(1+(interestRate/100), years)
-
 	fmt.Printf("Final Amount is: %.2f\n", finalAmount)
 
 	profit := finalAmount - principalAmount
-
 	fmt.Printf("Profit is: %.2f\n", profit)
+
+	inflationAdjustedValue := finalAmount / math.Pow(1 + (inflationRate / 100), years)
+	fmt.Printf("Inflation adjusted final amount is : %.2f \n", inflationAdjustedValue)
 
 	// fmt.Printf("After investing %v for %v years at an interest rate of %v %, the final amount is : %v\n", principalAmount, years, interestRate, finalAmount);
 }
