0cb276908d861c0793ada884f42df7d897ab55a0
Venkata Krishna Sai <venkat.in2002@gmail.com>
2025-06-27 10:16:56 +0530

    Error Handling

--- Course_tutorials/Profit_calculator/main.go
+++ Course_tutorials/Profit_calculator/main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main () {
+func main() {
 
 	var income, expenses, taxRate float64
 
@@ -15,13 +15,30 @@ func main () {
 	fmt.Print("Enter your tax rate (as a percentage): ")
 	fmt.Scan(&taxRate)
 
-	EarningsBeforetax := income - expenses
-	EarningsAfterTax := EarningsBeforetax * (1 - taxRate/100)
+	if income < expenses {
 
-	fmt.Printf("Your earnings before tax: %.2f\n", EarningsBeforetax)
-	fmt.Printf("Tax deducted is: %.2f\n", EarningsBeforetax - EarningsAfterTax)
-	fmt.Printf("Your profit after deducting tax: %.2f\n", EarningsAfterTax)
+		fmt.Println("Your expenses are greater than your income. You're losing money!")
+		return
 
-	ratio := EarningsAfterTax / income * 100
-	fmt.Printf("Your profit after tax as a percentage of your total income: %.2f%%\n", ratio)
+	} else if income > expenses && income >= 0 && expenses >= 0 {
+
+		EarningsBeforetax := income - expenses
+		EarningsAfterTax := EarningsBeforetax * (1 - taxRate/100)
+
+		fmt.Printf("Your earnings before tax: %.2f/-\n", EarningsBeforetax)
+		fmt.Printf("Tax deducted is: %.2f/-\n", EarningsBeforetax-EarningsAfterTax)
+		fmt.Printf("Your profit after deducting tax: %.2f/-\n", EarningsAfterTax)
+
+		ratio := EarningsAfterTax / income * 100
+		fmt.Printf("Your profit after tax as a percentage of your total income: %.2f%%\n", ratio)
+
+		return
+
+	} else {
+
+		fmt.Println("Please enter valid non-negative numbers for income and expenses.")
+		return
+
+	}
+
 }
