547bb33cd8fe040b2eeaa642596c58276f7e9380
Venkata Krishna Sai Miriyala <miriyala.venka@hcl.com>
2025-10-12 11:59:54 +0530

    Optimized the code with validation checks and removing Print() statements

--- Course_tutorials/5-bank-app/bank.go
+++ Course_tutorials/5-bank-app/bank.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-var initialAmt int = 10000
+var initialAmt float64 = 10000
 
 func main() {
 
@@ -19,13 +19,10 @@ func main() {
 
 	switch choice {
 	case 1:
-		fmt.Println("You've chosen option 1")
 		balanceCheck()
 	case 2:
-		fmt.Println("You've chosen option 2")
 		addMoney()
 	case 3:
-		fmt.Println("You've chosen option 3")
 		withdrawMoney()
 	case 4:
 		return
@@ -41,24 +38,34 @@ func balanceCheck() {
 }
 
 func addMoney() {
-	var deposit int
+	var deposit float64
 	fmt.Print("How much would you like to deposit? ")
 	fmt.Scan(&deposit)
 
-	initialAmt = initialAmt + deposit
+	if deposit <= 0 {
+		fmt.Println("You're depositing an invalid amount. Deposit amount must be greater than 0. Please try again")
+		return
+	}
+
+	initialAmt += deposit
 	fmt.Printf("You've successfully deposited %v/- into your account. Total Balance is: %v/- \n", deposit, initialAmt)
 }
 
 func withdrawMoney() {
-	var withdrawAmt int
+	var withdrawAmt float64
 	fmt.Print("How much would you like to withdraw? ")
 	fmt.Scan(&withdrawAmt)
+
+	if withdrawAmt <= 0 {
+		fmt.Println("You're withdrawing an invalid amount. Withdraw amount must be greater than 0. Please try again")
+		return
+	}
 
 	if withdrawAmt > initialAmt {
 		fmt.Println("You're withdrawing more amount than what is available in your account. Please try again")
 		return
 	}
 
-	initialAmt = initialAmt - withdrawAmt
+	initialAmt -= withdrawAmt
 	fmt.Printf("You've successfully withdrawn %v/- from your account. Remaining balance is: %v/- \n", withdrawAmt, initialAmt)
 }
